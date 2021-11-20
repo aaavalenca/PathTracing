795e226ec097bd57ccb154c8635f3ea9a08fe6dc
aaavalenca <aaav@cin.ufpe.br>
2021-11-20 17:46:01 -0300

    Created Vertex and Fragment shaders

--- main.cpp
+++ main.cpp
@@ -3,8 +3,48 @@
 #include <iostream>
 using namespace std;
 
-static int CreateShader(const string& vertexShader, const string& fragmentShader){
+static int CompileShader(unsigned int type, const string& source){
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
 
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if (result == GL_FALSE){
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        cout
+        << "Failed to compile "
+        << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+        << " shader!"
+        << endl;
+        cout << message << endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+
+static unsigned int CreateShader(const string& vertexShader, const string& fragmentShader){
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
 }
 
 int main(void)
@@ -46,6 +86,20 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2* sizeof(float), 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    string vertexShader = "#version 120\n"
+                          "attribute vec4 position;\n"
+                          "void main()\n"
+                          "{\n"
+                          "gl_Position = position;\n"
+                          "}";
+    string fragmentShader = "#version 120\n"
+                            "void main()\n"
+                            "{\n"
+                            "gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);\n"
+                            "}";
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
